2417078b1d1e145c66c724ffb26f58a4f2f1710a
nicolasdelfino <nicolas.delfino@tretton37.com>
2018-12-22 00:26:53 +0100

    compression

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const app = express();
 const path = require('path');
+const compression = require('compression');
 const es6Renderer = require('express-es6-template-engine')
 const PORT = process.env.PORT = 4000;
 
 app.engine('html', es6Renderer);
 app.set('views',path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(compression())
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
